0fb0d35352151e7bbea06180d4bef0001e4c62fe
Abubeker Mohammed <abukie969@gmail.com>
2020-06-10 18:20:34 +0800

    gh-pages removed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 const config = require('./src/config');
 
 module.exports = {
-  pathPrefix: '/my_project',
+  pathPrefix: '/bukiy.github.io',
   siteMetadata: {
     title: config.siteTitle,
     siteUrl: config.siteUrl,
